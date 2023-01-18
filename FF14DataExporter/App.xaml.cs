b93ed2c32f2ed90c6ae44c5d0f79f9a6a3b1db8b
James Lynch <eimi_christian@hotmail.com>
2023-01-17 22:38:46 -0600

    Here we go again!

---
+++ FF14DataExporter/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using FF14DataExporter.Views;
+
+using Prism.Ioc;
+
+using System.Windows;
+
+namespace FF14DataExporter;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+
+    }
+}
